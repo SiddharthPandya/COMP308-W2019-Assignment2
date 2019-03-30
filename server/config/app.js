fb5d0536e2e86e1062aad65be3f0dab62da1f949
Siddharth Pandya <sid.pandya66@gmail.com>
2019-03-30 02:46:27 -0400

    Added angular front-end to project

--- server/config/app.js
+++ server/config/app.js
@@ -98,8 +98,9 @@ passport.use(strategy);
 
 app.use('/api', indexRouter);
 app.use('/api/contact-list', contactRouter); //TODO: protect this section
-
-// TODO: need to capture random links or incorrect URL information
+app.get('*', (req,res) => {
+  res.sendfile(path.join(__dirname,'../../public/index.html'));
+});
 
 
 // catch 404 and forward to error handler
