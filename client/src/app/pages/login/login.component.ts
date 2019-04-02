c985fb54ac65ea972b8b856b1ea98b052e9de597
Siddharth Pandya <sid.pandya66@gmail.com>
2019-04-01 22:58:02 -0400

    added Register and Login

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.user = new User();
+  }
+
+  onLoginSubmit(): void {
+    this.authService.authenticateUser(this.user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show(data.msg, {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/home']);
+      } else {
+        this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/login']);
+      }
+    });
   }
 
 }
