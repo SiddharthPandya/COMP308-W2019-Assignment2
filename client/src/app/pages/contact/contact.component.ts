ac555eee7a367ba37edfcf85a0fd9793d06e450e
Siddharth Pandya <sid.pandya66@gmail.com>
2019-04-06 12:35:25 -0400

    updated contact page

--- client/src/app/pages/contact/contact.component.ts
+++ client/src/app/pages/contact/contact.component.ts
@@ -5,8 +5,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-contact',
@@ -16,7 +17,9 @@ import { AuthService } from 'src/app/services/auth.service';
 export class ContactComponent extends BasePageComponent implements OnInit {
 
   constructor(route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
     ) {
     super(route);
    }
@@ -26,6 +29,13 @@ export class ContactComponent extends BasePageComponent implements OnInit {
 
   isLoggedIn(): boolean{
     return this.authService.loggedIn();
+  }
+
+  onMessageSubmit(): void {
+        
+        this.flashMessage.show('Message Sent', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/home']);
+     
   }
 
 }
